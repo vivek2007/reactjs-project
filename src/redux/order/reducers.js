b3653e9e2db716e68043039171f5922ce5587c7f
vivek <rajvivek.v@gmail.com>
2020-08-07 17:42:41 +0530

    order-invoice-implemented

--- src/redux/order/reducers.js
+++ src/redux/order/reducers.js
@@ -1,7 +1,20 @@
+import store from 'store'
 import actions from './actions'
 
+const STORED_ORDER_DATA = orderData => {
+  const data = {}
+  Object.keys(orderData).forEach(key => {
+    const item = store.get(`app.order`)
+    data[key] = typeof item !== 'undefined' ? item[key] : orderData[key]
+  })
+  return data
+}
+
 const initialState = {
-  orderSuccess: false,
+  ...STORED_ORDER_DATA({
+    orderSuccess: false,
+    orderForInvoice: {},
+  }),
 }
 
 export default function orderReducer(state = initialState, action) {
